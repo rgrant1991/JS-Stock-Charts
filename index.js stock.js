dd8e147236edd61664f8836f16ceee0da4044fe9
rgrant1991 <115594492+rgrant1991@users.noreply.github.com>
2023-01-29 06:07:02 -0800

    Add files via upload

---
+++ index.js stock.js
@@ -0,0 +1,159 @@
+async function main() {
+
+    const timeChartCanvas = document.querySelector('#time-chart');
+    const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
+    const averagePriceChartCanvas = document.querySelector('#average-price-chart');
+
+}
+
+main()
+
+let GME = result.GME
+let MSFT = result.MSFT
+let DIS = result.DIS
+let BTNX = result.BTNX
+
+const stocks = [GME, MSFT, DIS, BNTX];
+
+// Bonus Note: 
+// Another way to write the above lines would to refactor it as:
+   // const {GME, MSFT, DIS, BTNX} = result 
+// This is an example of "destructuring" an object
+// "Destructuring" creates new variables from an object or an array
+
+const { GME, MSFT, DIS, BNTX } = mockData;
+
+const stocks = [GME, MSFT, DIS, BNTX];
+
+new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+new Chart(timeChartCanvas.getContext('2d'), {
+    type: 'line',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor:  'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)'
+        }]
+    }
+});
+
+console.log(stocks[0].values)                                                  
+
+new Chart(timeChartCanvas.getContext('2d'), {
+    type: 'line',
+    data: {
+        labels: stocks[0].values.map(value => value.datetime),
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor:  'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)'
+        }]
+    }
+});
+
+new Chart(timeChartCanvas.getContext('2d'), {
+    type: 'line',
+    data: {
+        labels: stocks[0].values.map(value => value.datetime),
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor:  'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)'
+        }]
+    }
+});
+
+new Chart(timeChartCanvas.getContext('2d'), {
+    type: 'line',
+    data: {
+        labels: stocks[0].values.map(value => value.datetime),
+        datasets: stocks.map( stock => ({
+            label: stock.meta.symbol,
+            data: _,
+            backgroundColor:  _,
+            borderColor: _,
+        }))
+    }
+});
+
+function getColor(stock){
+    if(stock === "GME"){
+        return 'rgba(61, 161, 61, 0.7)'
+    }
+    if(stock === "MSFT"){
+        return 'rgba(209, 4, 25, 0.7)'
+    }
+    if(stock === "DIS"){
+        return 'rgba(18, 4, 209, 0.7)'
+    }
+    if(stock === "BNTX"){
+        return 'rgba(166, 43, 158, 0.7)'
+    }
+}
+
+new Chart(timeChartCanvas.getContext('2d'), {
+    type: 'line',
+    data: {
+        labels: stocks[0].values.map(value => value.datetime),
+        datasets: stocks.map( stock => ({
+            label: stock.meta.symbol,
+            data: stock.values.map(value => parseFloat(value.high)),
+            backgroundColor:  getColor(stock.meta.symbol),
+            borderColor: getColor(stock.meta.symbol),
+        }))
+    }
+});
+
+stocks.forEach( stock => stock.values.reverse())
+
+// Time Chart
+new Chart(timeChartCanvas.getContext('2d'), {
+    type: 'line',
+    data: {
+        labels: stocks[0].values.reverse().map(value => value.datetime),
+        datasets: stocks.map(stock => ({
+            label: stock.meta.symbol,
+            data: stock.values.reverse().map(value => parseFloat(value.high))
+            backgroundColor: getColor(stock.meta.symbol),
+            borderColor: getColor(stock.meta.symbol),
+        }))
+    }
+});
